ecc54f64b7807a8471d69d04e81f0adfe3a8ff2b
Jan Neukirchen <jgn@posteo.de>
2021-08-26 17:09:02 +0200

    prevent translateAllEnDe from overwriting files

--- translateAllEnDe.py
+++ translateAllEnDe.py
@@ -6,17 +6,23 @@ from html import unescape
 
 
 ORIGINAL_TEXTFILE_FOLDER = '/Users/jan/Nextcloud/Art-Pilot/Ausstellungsplanung Übersicht/Texte für ArtPilot/generated/' + configuration.RUN_NUMBER + "/"
-ENGLISH_TEXTFILE_FOLDER = '/Users/jan/Nextcloud/Art-Pilot/Ausstellungsplanung Übersicht/Texte für ArtPilot/generated-translated/' + configuration.RUN_NUMBER + "/"
-pathlib.Path(ENGLISH_TEXTFILE_FOLDER).mkdir(parents=True, exist_ok=True)
+OUTPUT_TEXTFILE_FOLDER = '/Users/jan/Nextcloud/Art-Pilot/Ausstellungsplanung Übersicht/Texte für ArtPilot/generated-translated/' + configuration.RUN_NUMBER + "/"
+pathlib.Path(OUTPUT_TEXTFILE_FOLDER).mkdir(parents=True, exist_ok=True)
 
 for filename in os.listdir(ORIGINAL_TEXTFILE_FOLDER):
     if filename.endswith(".txt"): 
+        # skip existing output files
+        output_filename = OUTPUT_TEXTFILE_FOLDER + filename
+        if os.path.isfile(output_filename):
+            print(f'error: file {output_filename} already exists')
+            continue
+
+        # read and translate input file and write output file
         with open(ORIGINAL_TEXTFILE_FOLDER + filename, 'r') as reader:
             text = reader.read()
             translation = translate.translate_text(text, "de")
             translation_fixed = unescape(translation)
-
-            with open(ENGLISH_TEXTFILE_FOLDER + filename, 'w') as writer:
+            with open(output_filename, 'w') as writer:
                 writer.write(translation_fixed)
         continue
     else:
