2bdab647bebbe673b333cf1e9666b75f0832d51c
Jan Neukirchen <jgn@posteo.de>
2021-08-31 00:48:37 +0200

    refactoring

--- text2speech.py
+++ text2speech.py
@@ -1,6 +1,7 @@
 import boto3
 import pathlib
 import os
+import pathlib
 
 polly_client = boto3.Session().client('polly')
 
@@ -26,3 +27,28 @@ def text_to_mp3(text, output_file_name, voice='Vicki'):
     written_bites = file.tell()
     file.close()
     return written_bites # return file size in bytes
+
+def speak_folder(input_folder, output_folder):
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".txt"): 
+            with open(os.path.join(input_folder, filename), 'r') as reader:
+                text = reader.read()
+                new_filename = filename[:-4] + ".mp3"
+                text_to_mp3(text, os.path.join(output_folder, new_filename))
+            print(f"created file {new_filename}")
+            continue
+        else:
+            continue
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Translate a file and write the translation into another file')
+    parser.add_argument('inputfolder_name', help='name of the folder with the input files')
+    parser.add_argument('outputfolder_name', help='name of the folder where the output files will be written to')
+    parser.add_argument('subfolder_name', help='name of the output subfolder to create')
+
+    args = parser.parse_args()
+
+    subfolder_name = args.subfolder_name
+    input_folder = os.path.join(args.inputfolder_name, args.subfolder_name)
+    output_folder = os.path.join(args.outputfolder_name, args.subfolder_name)
+    speak_folder(input_folder, output_folder)
--- translate.py
+++ translate.py
@@ -2,6 +2,8 @@ import os
 import six
 from google.cloud import translate_v2 as translate
 from html import unescape
+import argparse
+import pathlib
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'../aqueous-timer-323814-bc940f9d191f.json'
 
@@ -22,3 +24,39 @@ def translate_text(text, target_language):
     result = translate_client.translate(text, target_language=target_language, format_="text")
 
     return result["translatedText"]
+
+def translate_folder(input_folder, output_folder, language):
+    pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".txt"): 
+            # skip existing output files
+            output_filename = os.path.join(output_folder, filename)
+            if os.path.isfile(output_filename):
+                print(f'error: file {output_filename} already exists')
+                continue
+
+            # read and translate input file and write output file
+            with open(os.path.join(input_folder, filename), 'r') as reader:
+                text = reader.read()
+                translation = translate_text(text, language)
+                translation_fixed = unescape(translation)
+                with open(output_filename, 'w') as writer:
+                    writer.write(translation_fixed)
+                    print(f"translated {filename} to {language}")
+            continue
+        else:
+            continue
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Translate a file and write the translation into another file')
+    parser.add_argument('inputfolder_name', help='name of the folder with the input files')
+    parser.add_argument('outputfolder_name', help='name of the folder where the output files will be written to')
+    parser.add_argument('language', help='the output language in ISO format')
+    parser.add_argument('subfolder_name', help='name of the output subfolder to create')
+
+    args = parser.parse_args()
+
+    input_folder = os.path.join(args.inputfolder_name, args.subfolder_name)
+    output_folder = os.path.join(args.outputfolder_name, args.subfolder_name)
+    language = args.language
+    translate_folder(input_folder, output_folder, language)
